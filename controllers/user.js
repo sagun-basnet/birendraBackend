08d1c1bc50ae9a3cc4db62be05b58db6e2d4015f
sagun basnet <basnetsagun8@gmail.com>
2025-04-23 16:35:16 +0545

    database CRUD operation

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,48 @@
-export const getUser = (req, res) => {
-  console.log("hello");
+import { db } from "../database/db.js";
 
-  res.send("I am from getUser");
+export const postUser = (req, res) => {
+  const { name, address, email, password } = req.body;
+
+  const q =
+    "insert into users(`name`, `address`, `email`, `password`) values (?,?,?,?)";
+
+  db.query(q, [name, address, email, password], (error, result) => {
+    if (error) return res.send(error);
+    return res.send({ message: "User has been created", result });
+  });
 };
-export const postUser = (req, res) => {};
 
 
+// axios.get("http://localhost:5055/api/get-user-by-id/${id}")
+export const getUserById = (req, res) => {
+  const id = req.params.pid;
+  const q = "select * from users where id = ?";
+  db.query(q, [id], (error, result) => {
+    if (error) return res.status(500).send(error);
+    return res.status(200).send(result[0]);
+  });
+};
 
+export const deleteUser = (req, res) => {
+  const id = req.params.id;
+  const q = "delete from users where id = ?";
+  db.query(q, [id], (error, result) => {
+    if (error) return res.status(500).send(error);
+    return res.status(200).send("User has been deleted");
+  });
+};
+
+export const updateUser = (req, res) => {
+  const id = req.params.id;
+  const { name, address, email, password } = req.body;
+  const q =
+    "update users set `name` = ?, `address` = ?, `email` = ?, `password` = ? where id = ?";
+  db.query(q, [name, address, email, password, id], (error, result) => {
+    if (error) return res.status(500).send(error);
+    return res.status(200).send("User has been updated");
+  });
+};
+
+result.length === 0;
+
+export const login = (req, res) => {};
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { getUser, postUser } from "../controllers/user.js";
+import {
+  getUser,
+  postUser,
+  getUserById,
+  deleteUser,
+  updateUser,
+} from "../controllers/user.js";
 import { checkUser } from "../middleware/checkuser.js";
 const router = express.Router();
 
 router.get("/getUser", getUser);
-router.post("/postUser", checkUser, postUser);
+router.post("/postUser", postUser);
+router.get("/get-user-by-id/:pid", getUserById);
+router.post("/delete-user/:id", deleteUser);
+router.post("/update-user/:id", updateUser);
 
 export default router;
